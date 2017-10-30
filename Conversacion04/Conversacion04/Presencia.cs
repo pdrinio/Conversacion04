5d089a30e9ec05024fa34a1f74f7d322cd0f4c39
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-10-30 18:16:15 +0100

    Añado presencia

---
+++ Conversacion04/Conversacion04/Presencia.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conversacion04
+{
+    public class Presencia
+    {
+        public enum Habitantes { Tani, Pablo, César, David, Javi, Iria, Fer, Pedro };
+        public List<Habitantes> Personas;
+
+
+        public Presencia()
+        {
+
+        }
+
+        public void Add(string Entrante)
+        {
+            Habitantes HabitanteEntrante;
+
+            if (Enum.TryParse<Habitantes>(Entrante, out HabitanteEntrante))
+            {
+                if (!Personas.Contains(HabitanteEntrante))
+                    Personas.Add(HabitanteEntrante);
+            }
+
+        }
+
+        public void Remove(Habitantes HabitanteSaliente)
+        {
+            if (Personas.Contains(HabitanteSaliente))
+                Personas.Remove(HabitanteSaliente);
+        }
+
+        public int Contains()
+        {
+            return Personas.Count;
+        }
+
+        public static List<string> ListaHabitantes()
+        {
+            List<string> _Personas = Enum.GetNames(typeof(Habitantes)).ToList();
+            List<string> _PersonasMayusculas = new List<string>();
+
+            foreach (string Persona in _Personas)
+            {
+                _PersonasMayusculas.Add(Persona.ToUpper());
+            }
+
+            return _PersonasMayusculas;
+        }
+    }
+
+}
